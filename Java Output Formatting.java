f3c4533d966954c77b6cfb304eb0dbc55083955f
Neyavanan V <55679009+Neyavanan@users.noreply.github.com>
2021-09-26 09:07:27 +0530

    Update Java Output Formatting.java

--- Java Output Formatting.java
+++ Java Output Formatting.java
@@ -1,5 +1,5 @@
 /*
-a portion of the solution is provided for you in the editor; you must format and print the input to complete the solution.
+of the solution is provided for you in the editor; you must format and print the input to complete the solution.
 
 Input Format
 

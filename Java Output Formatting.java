11bc98e02bf74c7aeab0fcaab07fc949d10b00f1
Neyavanan V <55679009+Neyavanan@users.noreply.github.com>
2021-10-07 07:37:02 +0530

    Update Java Output Formatting.java

--- Java Output Formatting.java
+++ Java Output Formatting.java
@@ -1,6 +1,5 @@
 /*
-
-Every line of input will contain a String followed by an integer.
+ine of input will contain a String followed by an integer.
 Each String will have a maximum of
 alphabetic characters, and each integer will be in the inclusive range from to
 

aed420b7044158858ac19bb64012b933e080944d
Neyavanan V <55679009+Neyavanan@users.noreply.github.com>
2021-11-06 19:28:58 +0530

    Update Java Output Formatting.java

--- Java Output Formatting.java
+++ Java Output Formatting.java
@@ -1,6 +1,5 @@
 /*
-
-The first column contains the String and is left justified using exactly
+first column contains the String and is left justified using exactly
 characters.
 The second column contains the integer, expressed in exactly
 

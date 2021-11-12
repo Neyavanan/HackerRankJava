ae767f4305af84c53cc49c0cf2142d11317c501a
Neyavanan V <55679009+Neyavanan@users.noreply.github.com>
2021-11-12 09:58:20 +0530

    Update Java Output Formatting.java

--- Java Output Formatting.java
+++ Java Output Formatting.java
@@ -1,5 +1,5 @@
 /*
-contains characters.
+characters.
 The second column contains the integer, expressed in exactly
 
 digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.

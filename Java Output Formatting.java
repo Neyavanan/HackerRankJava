e7a576e3756a12ed97150f92878e6cfa4e944fdf
Neyavanan V <55679009+Neyavanan@users.noreply.github.com>
2022-01-01 21:21:55 +0530

    Update Java Output Formatting.java

--- Java Output Formatting.java
+++ Java Output Formatting.java
@@ -1,7 +1,5 @@
 /*
-
-
-Each String is left-justified with trailing whitespace through the first
+left-justified with trailing whitespace through the first
 characters. The leading digit of the integer is the character, and each integer that was less than digits now has leading zeroes.
 
 */

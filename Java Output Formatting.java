540513309ab3cf1f1fe3d1fd0efcdb42cdd78ff4
Neyavanan V <55679009+Neyavanan@users.noreply.github.com>
2022-01-05 21:27:21 +0530

    Update Java Output Formatting.java

--- Java Output Formatting.java
+++ Java Output Formatting.java
@@ -1,5 +1,5 @@
 /*
-trailing whitespace through the first
+through the first
 characters. The leading digit of the integer is the character, and each integer that was less than digits now has leading zeroes.
 
 */

1c488aed4a548c9ae3d2e35431df4f789214d0de
Neyavanan V <55679009+Neyavanan@users.noreply.github.com>
2022-02-06 20:15:34 +0530

    Update Java Stdin and Stdout I.java

--- Java Stdin and Stdout I.java
+++ Java Stdin and Stdout I.java
@@ -1,5 +1,5 @@
 /*
- HackerRank challenges require you to read input from stdin (standard input) and write output to stdout (standard output).
+challenges require you to read input from stdin (standard input) and write output to stdout (standard output).
 
 One popular way to read input from stdin is by using the Scanner class and specifying the Input Stream as System.in. For example:
 

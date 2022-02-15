e1bb43b938c270551201554ef70525975e0c202d
Neyavanan V <55679009+Neyavanan@users.noreply.github.com>
2022-02-15 23:13:27 +0530

    Update Java Stdin and Stdout II.java

--- Java Stdin and Stdout II.java
+++ Java Stdin and Stdout II.java
@@ -1,7 +1,7 @@
 /*
 In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below. To make the problem a little easier, a portion of the code is provided for you in the editor.
 
-Note: We recommend completing Java Stdin and Stdout I before attempting this challenge.
+e recommend completing Java Stdin and Stdout I before attempting this challenge.
 
 Input Format
 
